ac8d7dc8da05d45a5533bdd910724924dc3509e6
Cem <cemkgrbz@gmail.com>
2022-06-29 13:26:53 +0200

    modified js file

--- solution.js
+++ solution.js
@@ -1,1 +1,33 @@
 // write your code in this file
+
+// 1
+
+console.log('This is an exercise in console logging')
+
+// 2
+
+console.log('------------')
+
+const exercise = 'This is the value.';
+console.log(exercise)
+
+// 3
+
+console.log('------------')
+const firstName = 'Cem Kaan';
+const lastName = 'Gürbüz';
+console.log(firstName,lastName)
+
+//4
+
+console.log('------------')
+const city = 'Leipzig';
+const job = 'unemployed';
+const age = '32';
+console.log(firstName, lastName, age, city, job)
+
+//5
+
+console.log('------------')
+console.log(firstName, lastName + ' is a ' + age + ' years old ' + job + ', who lives in ' + city)
+
